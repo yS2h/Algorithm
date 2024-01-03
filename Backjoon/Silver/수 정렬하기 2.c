3c0d26424df5ff172c85c44c1f5d0508c984f641
surusue <ysh@knu.ac.kr>
2024-01-03 11:57:24 +0900

    [2751] 수 정렬하기 2

---
+++ Backjoon/Silver/수 정렬하기 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b);
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    int * arr = (int *)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+
+    qsort(arr, N, sizeof(int), compare);
+
+    for (int i = 0; i < N; i++)
+        printf("%d\n", arr[i]);
+    return 0;
+}
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
