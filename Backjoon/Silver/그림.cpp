cf0fc647e0ecfbd1fd08c4e0cc172bc2bacd2a86
suhyeon yun <ysh@knu.ac.kr>
2024-02-20 20:26:54 +0900

    [1926] 그림

--- Backjoon/Silver/그림.cpp
+++ Backjoon/Silver/그림.cpp
@@ -7,7 +7,8 @@ bool vis[502][502];
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 int n, m;
-int main(){
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -29,8 +30,9 @@ int main(){
             int S = 0;
             while(!Q.empty()){
                 S++;
-                pair<int,int> cur = Q.front(); Q.pop();
-                for(int dir = 0; dir < 4; dir++){
+                pair<int,int> cur = Q.front();
+                Q.pop();
+                for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
                     if (nx < 0 || nx >= n || ny < 0 || ny >= m)
