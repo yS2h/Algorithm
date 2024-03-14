a8572255e9f3b06e9d33e2aec691c61c2610a830
surusue <ysh@knu.ac.kr>
2024-03-14 21:08:40 +0900

    [24051] 삽입 정렬 1

---
+++ Backjoon/Bronze/삽입 정렬 1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int insertion_sort(vector<int>& A, int K);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    cout << insertion_sort(A, K);
+
+    return 0;
+}
+
+int insertion_sort(vector<int>& A, int K) {
+    int cnt = 0;
+    for (int i = 1; i < A.size(); i++) {
+        int loc = i - 1;
+        int newItem = A[i];
+
+        while (0 <= loc && newItem < A[loc]) {
+            A[loc + 1] = A[loc];
+            loc--;
+            cnt++;
+            if (cnt == K) {
+                return A[loc + 1];
+            }
+        }
+        if (loc + 1 != i) {
+            A[loc + 1] = newItem;
+            cnt++;
+            if (cnt == K)
+                return A[loc + 1];
+        }
+    }
+    return -1;
+}
