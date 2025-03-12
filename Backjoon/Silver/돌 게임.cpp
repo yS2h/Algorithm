1fc366f0b5e1991c805d5f35a61b5f2d06b83288
윤수현 <ysh@knu.ac.kr>
2025-03-12 14:35:29 +0900

    [9655] 돌 게임

---
+++ Backjoon/Silver/돌 게임.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    if (N % 2)
+        cout << "SK";
+    else
+        cout << "CY";
+
+    return 0;
+}
